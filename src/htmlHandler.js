d26f5c2918d4a8f211e938c3528a06fe78eae41b
JonStudentRIT <jk9927@g.rit.edu>
2023-02-20 14:30:38 -0500

    basic server controls added, htmlHandler basic functions added, ref for documentation https://www.geeksforgeeks.org/what-is-long-polling-and-short-polling/

---
+++ src/htmlHandler.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../index.html`);
+const css = fs.readFileSync(`${__dirname}/../style.css`);
+
+// general html response function
+const wrightResponse = (response, fileType, type) => {
+  response.writeHead(200, { 'Content-Type': type });
+  response.write(fileType);
+  response.end();
+};
+
+// get the home page
+const getIndex = (request, response) => {
+  wrightResponse(response, index, 'text/html');
+};
+
+// get the css
+const getCSS = (request, response) => {
+  wrightResponse(response, css, 'text/css');
+};
+
+module.exports = {
+  getIndex,
+  getCSS,
+};
--- src/server.js
+++ src/server.js
@@ -5,3 +5,10 @@ const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+};
+
+http.createServer(onRequest).listen(port);
