7a6eacb570eef739606cc3c867a4ddb4d0b1e246
JonStudentRIT <jk9927@g.rit.edu>
2023-02-26 14:42:42 -0500

    Basic changes mostly just comments, added onRequest function to open the server to work with heroku

--- src/server.js
+++ src/server.js
@@ -1,14 +1,27 @@
 const http = require('http');
 const url = require('url');
 
-const htmlHandler = require('./htmlResponses.js');
-const jsonHandler = require('./jsonResponses.js');
+const htmlHandler = require('./htmlHandler.js');
+const jsonHandler = require('./jsonHandler.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+//polling 
+//https://www.geeksforgeeks.org/what-is-long-polling-and-short-polling/
+//https://javascript.info/long-polling
 const urlStruct = {
-    '/': htmlHandler.getIndex,
-    '/style.css': htmlHandler.getCSS,
+    POST: {},
+    GET: {
+      '/': htmlHandler.getIndex,
+      '/style.css': htmlHandler.getCSS,
+    },
+    HEAD: {},
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  console.log(parsedUrl);
+  htmlHandler.getIndex(request, response);
 };
 
 http.createServer(onRequest).listen(port);
