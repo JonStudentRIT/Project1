5673f541a2e5d95c5ee3c47ee8ae6b1187789c57
JonStudentRIT <jk9927@g.rit.edu>
2023-03-05 14:39:45 -0500

    Linter corrections made

--- src/jsonHandler.js
+++ src/jsonHandler.js
@@ -1,183 +1,172 @@
-const { request } = require('http');
 const query = require('querystring');
 
 // the two players
-let player1 = { index : 1, active : true, selected : false };
-let player2 = { index : 2, active : false, selected : false };
+const player1 = { index: 1, active: true, selected: false };
+const player2 = { index: 2, active: false, selected: false };
 
 // the gameBoard being sent through the clients and the server
 let gameBoard = '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0';
 
 // general json response get function
 const respondJSON = (request, response, status, object) => {
-    const headers = { 'Content-Type': 'application/json' };
-    response.writeHead(status, headers);
-    response.write(JSON.stringify(object));
-    response.end();
+  const headers = { 'Content-Type': 'application/json' };
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
 };
 
 // general json response head function
 const respondJSONMeta = (request, response, status) => {
-    const headers = { 'Content-Type': 'application/json' };
-    response.writeHead(status, headers);
-    response.end();
-  };
+  const headers = { 'Content-Type': 'application/json' };
+  response.writeHead(status, headers);
+  response.end();
+};
 
 // the client is assigned a role in the game
 const playerInit = (request, response) => {
-    let responseJSON = {message: ' '};
-    // player one is set
-    if(player1.selected === false)
-    {
-        responseJSON.message = '1';
-        player1.selected = true;
-        return respondJSON(request, response, 201, responseJSON);
-    }
-    // player two is set
-    else if (player2.selected === false)
-    {
-        responseJSON.message = '2';
-        player2.selected = true;
-        return respondJSON(request, response, 201, responseJSON);
-    }
-    // all additional people are spectators 
-    else
-    {
-        responseJSON.message = '3';
-        return respondJSON(request, response, 201, responseJSON);
-    }
-}
+  const responseJSON = { message: ' ' };
+  // player one is set
+  if (player1.selected === false) {
+    responseJSON.message = '1';
+    player1.selected = true;
+    return respondJSON(request, response, 201, responseJSON);
+  }
+  // player two is set
+  if (player2.selected === false) {
+    responseJSON.message = '2';
+    player2.selected = true;
+    return respondJSON(request, response, 201, responseJSON);
+  }
+  // all additional people are spectators
+
+  responseJSON.message = '3';
+  return respondJSON(request, response, 201, responseJSON);
+};
 
 // get the board status and the current players turn
 const getTick = (request, response) => {
-    const responseJSON = {player: player1.active, board: gameBoard};
-    return respondJSON(request, response, 200, responseJSON);
-};
-
-// look to see if the player under "player=#" has won
-const getWin = (request, response, params) => {
-    let responseJSON = {};
-    if(params.player)
-    {
-        // is the player a player
-        if(params.player == '1' || params.player == '2')
-        {
-            let didThisPLayerWin = searchWinCondition(params.player);
-            responseJSON = {winner: didThisPLayerWin};
-            return respondJSON(request, response, 200, responseJSON);
-        }
-        // or a spectator
-        responseJSON.message = 'The player Identity in this case is a spectator not a player',
-        responseJSON.id = 'notAPlayer'
-        return respondJSON(request, response, 400, responseJSON);
-    }
-    responseJSON.message = 'The player parameter is missing',
-    responseJSON.id = 'missingPlayer'
-    return respondJSON(request, response, 400, responseJSON);
-};
-
-const updateGame = (request, response) => {
-    let body = [];
-
-    request.on('data', (chunk) => {
-    body.push(chunk);
-    });
-
-    request.on('end', () => {
-        const bodyString = Buffer.concat(body).toString();
-        body = query.parse(bodyString);
-
-        // is the players move not permitted
-        if(body.valid === 'false')
-        {
-            return respondJSONMeta(request, response, 204);
-        }
-
-        // if it is permitted then who is the player moving
-        if(body.id === '1')
-        {
-            if(player1.active)
-            {
-                player1.active = false;
-                player2.active = true;
-            }
-        }
-        if(body.id === '2')
-        {
-            if(player2.active)
-            {
-                player1.active = true;
-                player2.active = false;
-            }
-        }
-        // update the board
-        gameBoard = body.board;
-        const responseJSON = { message: gameBoard };
-        return respondJSON(request, response, 201, responseJSON);
-    });
+  const responseJSON = { player: player1.active, board: gameBoard };
+  return respondJSON(request, response, 200, responseJSON);
 };
 
 // did the player reach a win condition
 const searchWinCondition = (player) => {
-    let board = [];
-    board = gameBoard.split(',');
-    let win = false;
-    // horizontal
-    if (board[12] == player && board[13] == player && board[14] == player && board[15] == player) {
-        win = true;
+  let board = [];
+  board = gameBoard.split(',');
+  let win = false;
+  // horizontal
+  // linter wont accept the old format for this line for some reason but does accep this ¯\_(ツ)_/¯
+  if (board[12] === player && board[13] === player
+    && board[14] === player && board[15] === player) {
+    win = true;
+  }
+  if (board[8] === player && board[9] === player && board[10] === player && board[11] === player) {
+    win = true;
+  }
+  if (board[4] === player && board[5] === player && board[6] === player && board[7] === player) {
+    win = true;
+  }
+  if (board[0] === player && board[1] === player && board[2] === player && board[3] === player) {
+    win = true;
+  }
+  // vertical
+  if (board[15] === player && board[11] === player && board[7] === player && board[3] === player) {
+    win = true;
+  }
+  if (board[14] === player && board[10] === player && board[6] === player && board[2] === player) {
+    win = true;
+  }
+  if (board[13] === player && board[9] === player && board[5] === player && board[1] === player) {
+    win = true;
+  }
+  if (board[12] === player && board[8] === player && board[4] === player && board[0] === player) {
+    win = true;
+  }
+  // left Cross
+  if (board[12] === player && board[9] === player && board[6] === player && board[3] === player) {
+    win = true;
+  }
+  // right cross
+  if (board[15] === player && board[10] === player && board[5] === player && board[0] === player) {
+    win = true;
+  }
+  return win;
+};
+
+// look to see if the player under "player=#" has won
+const getWin = (request, response, params) => {
+  let responseJSON = {};
+  if (params.player) {
+    // is the player a player
+    if (params.player === '1' || params.player === '2') {
+      const didThisPLayerWin = searchWinCondition(params.player);
+      responseJSON = { winner: didThisPLayerWin };
+      return respondJSON(request, response, 200, responseJSON);
+    }
+    // or a spectator
+    responseJSON.message = 'The player Identity in this case is a spectator not a player';
+    responseJSON.id = 'notAPlayer';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+  responseJSON.message = 'The player parameter is missing';
+  responseJSON.id = 'missingPlayer';
+  return respondJSON(request, response, 400, responseJSON);
+};
+
+const updateGame = (request, response) => {
+  let body = [];
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    body = query.parse(bodyString);
+
+    // is the players move not permitted
+    if (body.valid === 'false') {
+      return respondJSONMeta(request, response, 204);
+    }
+
+    // if it is permitted then who is the player moving
+    if (body.id === '1') {
+      if (player1.active) {
+        player1.active = false;
+        player2.active = true;
       }
-      if (board[8] == player && board[9] == player && board[10] == player && board[11] == player) {
-        win = true;
+    }
+    if (body.id === '2') {
+      if (player2.active) {
+        player1.active = true;
+        player2.active = false;
       }
-      if (board[4] == player && board[5] == player && board[6] == player && board[7] == player) {
-        win = true;
-      }
-      if (board[0] == player && board[1] == player && board[2] == player && board[3] == player) {
-        win = true;
-      }
-      // vertical
-      if (board[15] == player && board[11] == player && board[7] == player && board[3] == player) {
-        win = true;
-      }
-      if (board[14] == player && board[10] == player && board[6] == player && board[2] == player) {
-        win = true;
-      }
-      if (board[13] == player && board[9] == player && board[5] == player && board[1] == player) {
-        win = true;
-      }
-      if (board[12] == player && board[8] == player && board[4] == player && board[0] == player) {
-        win = true;
-      }
-      // left Cross
-      if (board[12] == player && board[9] == player && board[6] == player && board[3] == player) {
-        win = true;
-      }
-      // right cross
-      if (board[15] == player && board[10] == player && board[5] == player && board[0] == player) {
-        win = true;
-      }
-      return win;
+    }
+    // update the board
+    gameBoard = body.board;
+    const responseJSON = { message: gameBoard };
+    return respondJSON(request, response, 201, responseJSON);
+  });
 };
 
 // was the page not found
 const notFound = (request, response) => {
-    const responseJSON = {
-      message: 'The page you are looking for was not found.',
-      id: 'notFound',
-    };
-    respondJSON(request, response, 404, responseJSON);
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
   };
+  respondJSON(request, response, 404, responseJSON);
+};
 
 const notFoundMeta = (request, response) => {
-    respondJSONMeta(request, response, 404);
-  };
+  respondJSONMeta(request, response, 404);
+};
 
 module.exports = {
-    getTick,
-    updateGame,
-    playerInit,
-    getWin,
-    notFound,
-    notFoundMeta,
+  getTick,
+  updateGame,
+  playerInit,
+  getWin,
+  notFound,
+  notFoundMeta,
 };
-  
--- src/server.js
+++ src/server.js
@@ -2,27 +2,27 @@ const http = require('http');
 const url = require('url');
 const query = require('querystring');
 
-const htmlHandler = require('./htmlHandler.js');
-const jsonHandler = require('./jsonHandler.js');
+const htmlHandle = require('./htmlHandler');
+const jsonHandle = require('./jsonHandler');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 // list of all handled URLs by method
 const urlStruct = {
-    POST: {
-      '/updateOut': jsonHandler.updateGame,
-    },
-    GET: {
-      '/': htmlHandler.getIndex,
-      '/getTick' : jsonHandler.getTick,
-      '/getWinner' : jsonHandler.getWin,
-      '/init' : jsonHandler.playerInit,
-      '/style.css': htmlHandler.getCSS,
-      notFound: jsonHandler.notFound,
-    },
-    HEAD: {
-      notFound: jsonHandler.notFoundMeta,
-    },
+  POST: {
+    '/updateOut': jsonHandle.updateGame,
+  },
+  GET: {
+    '/': htmlHandle.getIndex,
+    '/getTick': jsonHandle.getTick,
+    '/getWinner': jsonHandle.getWin,
+    '/init': jsonHandle.playerInit,
+    '/style.css': htmlHandle.getCSS,
+    notFound: jsonHandle.notFound,
+  },
+  HEAD: {
+    notFound: jsonHandle.notFoundMeta,
+  },
 };
 
 // get the url
@@ -34,8 +34,7 @@ const onRequest = (request, response) => {
     urlStruct.HEAD.notFound(request, response);
   }
   // if the url and method are handled
-  if(urlStruct[request.method][parsedUrl.pathname])
-  {
+  if (urlStruct[request.method][parsedUrl.pathname]) {
     urlStruct[request.method][parsedUrl.pathname](request, response, params);
   } else {
     urlStruct[request.method].notFound(request, response);
